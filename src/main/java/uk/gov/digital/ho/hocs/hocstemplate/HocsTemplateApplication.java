714d88e4f0ab789ecbb486980034a02573f5d192
sathik <sathik.abdulrahman@digital.homeoffice.gov.uk>
2022-03-30 16:30:56 +0100

    Initial Commit

---
+++ src/main/java/uk/gov/digital/ho/hocs/hocstemplate/HocsTemplateApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.digital.ho.hocs.hocstemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HocsTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HocsTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/digital/ho/hocs/hocstemplate/HocsTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.digital.ho.hocs.hocstemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HocsTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
